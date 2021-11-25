716364b11578a43770daa1596677d3a023ab6156
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2021-11-26 01:32:36 +0530

    blogs, cart

---
+++ Raw_PHP/_blogs.php
@@ -0,0 +1,35 @@
+ <!-- Blogs -->
+ <section id="blogs">
+   <div class="container py-4">
+     <h4 class="font-rubik font-size-28">𝔏𝔞𝔱𝔢𝔰𝔱 𝔅𝔩𝔬𝔤𝔰</h4>
+     <hr>
+
+     <div class="owl-carousel owl-theme">
+       <div class="item">
+         <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+           <h5 class="card-title font-size-16">𝗩𝗲𝗴𝗲𝘁𝗮𝗯𝗹𝗲 𝗧𝗶𝗺𝗲𝘀</h5>
+           <img src="../assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+           <p class="card-text font-size-14 text-black-50 py-1">𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑔𝑖𝑣𝑒 𝑦𝑜𝑢 𝑢𝑝, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑙𝑒𝑡 𝑦𝑜𝑢 𝑑𝑜𝑤𝑛, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑟𝑢𝑛 𝑎𝑟𝑜𝑢𝑛𝑑 𝑎𝑛𝑑 𝑑𝑒𝑠𝑒𝑟𝑡 𝑦𝑜𝑢, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑚𝑎𝑘𝑒 𝑦𝑜𝑢 𝑐𝑟𝑦, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑠𝑎𝑦 𝑔𝑜𝑜𝑑𝑏𝑦𝑒, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑡𝑒𝑙𝑙 𝑎 𝑙𝑖𝑒 𝑎𝑛𝑑 ℎ𝑢𝑟𝑡 𝑦𝑜𝑢!</p>
+           <a href="#" class="color-second text-left">𝗥𝗲𝗮𝗱 𝗠𝗼𝗿𝗲</a>
+         </div>
+       </div>
+       <div class="item">
+         <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+           <h5 class="card-title font-size-16">𝗗𝗮𝗶𝗿𝘆 𝗙𝗮𝗻𝘁𝗮𝘀𝘆</h5>
+           <img src="../assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+           <p class="card-text font-size-14 text-black-50 py-1">𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑔𝑖𝑣𝑒 𝑦𝑜𝑢 𝑢𝑝, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑙𝑒𝑡 𝑦𝑜𝑢 𝑑𝑜𝑤𝑛, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑟𝑢𝑛 𝑎𝑟𝑜𝑢𝑛𝑑 𝑎𝑛𝑑 𝑑𝑒𝑠𝑒𝑟𝑡 𝑦𝑜𝑢, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑚𝑎𝑘𝑒 𝑦𝑜𝑢 𝑐𝑟𝑦, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑠𝑎𝑦 𝑔𝑜𝑜𝑑𝑏𝑦𝑒, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑡𝑒𝑙𝑙 𝑎 𝑙𝑖𝑒 𝑎𝑛𝑑 ℎ𝑢𝑟𝑡 𝑦𝑜𝑢!</p>
+           <a href="#" class="color-second text-left">𝗥𝗲𝗮𝗱 𝗠𝗼𝗿𝗲</a>
+         </div>
+       </div>
+       <div class="item">
+         <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+           <h5 class="card-title font-size-16">𝗠𝗲𝗱𝗶𝗰𝗶𝗻𝗮𝗹 𝗠𝘂𝗻𝗰𝗵𝗲𝘀</h5>
+           <img src="../assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+           <p class="card-text font-size-14 text-black-50 py-1">𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑔𝑖𝑣𝑒 𝑦𝑜𝑢 𝑢𝑝, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑙𝑒𝑡 𝑦𝑜𝑢 𝑑𝑜𝑤𝑛, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑟𝑢𝑛 𝑎𝑟𝑜𝑢𝑛𝑑 𝑎𝑛𝑑 𝑑𝑒𝑠𝑒𝑟𝑡 𝑦𝑜𝑢, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑚𝑎𝑘𝑒 𝑦𝑜𝑢 𝑐𝑟𝑦, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑠𝑎𝑦 𝑔𝑜𝑜𝑑𝑏𝑦𝑒, 𝑁𝑒𝑣𝑒𝑟 𝑔𝑜𝑛𝑛𝑎 𝑡𝑒𝑙𝑙 𝑎 𝑙𝑖𝑒 𝑎𝑛𝑑 ℎ𝑢𝑟𝑡 𝑦𝑜𝑢!</p>
+           <a href="#" class="color-second text-left">𝗥𝗲𝗮𝗱 𝗠𝗼𝗿𝗲</a>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
+ <!-- !Blogs -->
