6a9bfe39908941ac4e702561481ded7a16255477
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2021-11-25 23:50:34 +0530

    php updates! :)

--- Raw PHP/_banner-area.php
+++ Raw PHP/_banner-area.php
@@ -2,14 +2,25 @@
 <section id="banner-area">
     <div class="owl-carousel owl-theme">
         <div class="item">
-            <img src="./assets/Banner1.png" alt="Banner1">
+            <img src="../assets/Banner1.png" alt="Banner1">
         </div>
         <div class="item">
-            <img src="./assets/Banner2.png" alt="Banner2">
+            <img src="../assets/Banner2.png" alt="Banner2">
         </div>
         <div class="item">
-            <img src="./assets/Banner1.png" alt="Banner3">
+            <img src="../assets/Banner3.png" alt="Banner3">
         </div>
-    </div>
+        <div class="item">
+            <img src="../assets/Banner4.png" alt="Banner4">
+        </div>
+        <div class="item">
+            <img src="../assets/Banner5.png" alt="Banner5">
+        </div>
+        <div class="item">
+            <img src="../assets/Banner6.png" alt="Banner6">
+        </div>
+        <div class="item">
+            <img src="../assets/Banner7.png" alt="Banner7">
+        </div>
 </section>
 <!-- !Owl-carousel -->
