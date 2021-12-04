8454cd327dcd3f64ccbcbe51c0bcdd8f85b39c59
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2021-12-04 23:27:25 +0530

    fixed cart counter

--- header.php
+++ header.php
@@ -72,7 +72,7 @@
         <form action="#" class="font-size-14 font-rale">
           <a href="cart.php" class="py-2 rounded-pill color-primary-bg">
             <span class="font-size-16 px-2 text-dark"><i class="fas fa-shopping-cart"></i></span>
-            <span class="px-3 py-2 rounded-pill text-dark bg-light">0</span>
+            <span class="px-3 py-2 rounded-pill text-dark bg-light"><?php echo count($product->getData('cart')); ?></span>
           </a>
         </form>
       </div>
